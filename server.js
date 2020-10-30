61b60658fbbd3b7e3a7a877879aac5db5a2ce5bb
harshit2001sharma@gmail.com <44293679+Harshit123Sharma@users.noreply.github.com>
2020-10-30 23:04:58 +0530

    Second Commit!!!

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+var items = [];
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -7,26 +8,67 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.get("/", function(req, res){
+
+app.get("/", function(req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    //var currentDay = today.getDay();
+    var options = {
 
-    if (currentDay === 6 || currentDay === 0) {
+        weekday : "long",
+        year : "numeric",
+        month : "long",
+        day : "numeric"
 
-        day = "Weekend";
-        res.render("list", {DAY : day});
+    };
+    var day = today.toLocaleDateString("en-US", options); 
+    console.log(day);
+    /*switch (currentDay) {
+        case 0 : 
+            day = "Sunday";
+            break;
+        case 1 : 
+            day = "Monday";
+            break; 
+        case 2 : 
+            day = "Tuesday";
+            break;
+        case 3 : 
+            day = "Wedday";
+            break;
+        case 4 : 
+            day = "Thursday";
+            break;
+        case 5 : 
+            day = "Friday";
+            break;
+        case 6 : 
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error : Current day is : " + currentDay);
+            break;
+    }*/
 
-    }
+    res.render("list", {
+        
+        DAY : day,
+        LIST_ITEMS : items
+    
+    });
 
-    else {
+ 
+});
 
-        day = "Weekday";
-        res.render("list", {DAY : day});
+app.post("/", function(req, res) {
 
-    }
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+    //console.log(item); 
+
 
 });
 
